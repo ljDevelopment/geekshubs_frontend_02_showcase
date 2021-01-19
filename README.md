c49dd7ba6a0e25db117eaae646b62c222ee4a479
Luis Javier López Arredondo <ljDevelopment.6.28@gmail.com>
2021-01-19 23:57:58 +0100

    Readme and format redy to delivery

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# geekshubs_frontend_02_showcase
+#  2020-11 - Frontent - Challenge 2
+
+ - **Author**: Luis Javier López Arredondo
+ - **Technologies**:  HTML5, CSS3, GIT, JS ES6, DOM
+ - **Abstract**: Second challenge for the course *2020-11- Frontend* at [GeeksHubs](https://geekshubs.com/), create a cart with drag & drop. Description for the requirements of the challenge can be found in [Reto 2 - Showcase - BTC Frontend.pdf](Reto 2 - Showcase - BTC Frontend.pdf)
+
+### Evaluation notes
+  - Javascript ES6.
+  - Dynamic content based on data structure.
+  - Responsive.
+  - Drag & drop generic easy to reuse.
+  - Cart stata changes based on events.
+  - Extr: remove from cart.
--- js/cartItemView.js
+++ js/cartItemView.js
@@ -2,7 +2,7 @@
 class CartItemView {
 
 	constructor(cart, item, value) {
-		
+
 		this.cart = cart;
 		this.item = item;
 		this.value = value;
@@ -12,28 +12,26 @@ class CartItemView {
 	}
 
 	_createView() {
-		
+
 		const cartItemId = 'cart' + this.item;
 
+		this.itemView = document.createElement('li');
+		this.itemView.id = cartItemId;
+		this.itemView.className = "cartListItem";
+		this.itemView.innerHTML = document.getElementById(this.item).innerHTML;
 
+		this.spanPartial = document.createElement('span');
+		this.spanPartial.id = this.item + 'Partial';
+		this.itemView.appendChild(this.spanPartial);
 
-			this.itemView = document.createElement('li');
-			this.itemView.id = cartItemId;
-			this.itemView.className = "cartListItem";
-			this.itemView.innerHTML = document.getElementById(this.item).innerHTML;
+		const removeButton = document.createElement('button');
+		removeButton.type = 'button';
+		removeButton.innerHTML = '-';
+		removeButton.addEventListener('click', () => this.cart.substract(this.item, this.value))
+		this.itemView.appendChild(removeButton);
 
-			this.spanPartial = document.createElement('span');
-			this.spanPartial.id = this.item + 'Partial';
-			this.itemView.appendChild(this.spanPartial);
-
-			const removeButton = document.createElement('button');
-			removeButton.type = 'button';
-			removeButton.innerHTML = '-';
-			removeButton.addEventListener('click', () => this.cart.substract(this.item, this.value))
-			this.itemView.appendChild(removeButton);
-
-			const parent = document.querySelector('#cart > ul'); 
-			parent.appendChild(this.itemView);
+		const parent = document.querySelector('#cart > ul');
+		parent.appendChild(this.itemView);
 	}
 
 	updateItemPartial() {
